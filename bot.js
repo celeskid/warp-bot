e4c8759082042c9684ed821644ed7688da6f4149
celeskid <40873127+celeskid@users.noreply.github.com>
2018-07-05 16:43:49 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
+client.login(process.env.NDY0MTM1OTU0ODUzMDAzMjY1.Dh6kxQ.62DfyLhTpnyMPmwgmO-FsC2kEUg);//where BOT_TOKEN is the token of our bot
